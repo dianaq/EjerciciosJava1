d5027a1e2f3427a70e0a45fe1a94403e8bf321fd
dianaquijije <dianaquije_1992@hotmail.com>
2014-06-28 10:43:22 -0500

    ejercicio1new

--- src/NewClass.java
+++ src/NewClass.java
@@ -4,19 +4,10 @@
  * and open the template in the editor.
  */
 
-package ejerciciosjava1;
-
 /**
  *
  * @author Diana
  */
-public class EjerciciosJava1 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class NewClass {
     
 }
